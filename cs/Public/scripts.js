afc4406c5f22a70cf582fe8e9727b7f0db4c778d
Cloudstitch Services <services@cloudstitch.com>
2017-04-08 00:04:44 +0100

    [Cloudstitch Services] Project publish papodaca/new-simple-site

--- cs/Public/scripts.js
+++ cs/Public/scripts.js
@@ -1,5 +1,7 @@
 module.exports.beforeRender = function(elem, data) {
+  console.log("Hi there, before render!");
 }
 
 module.exports.afterRender = function(elem, data) {
+  console.log("Hi there, after render!");
 }
